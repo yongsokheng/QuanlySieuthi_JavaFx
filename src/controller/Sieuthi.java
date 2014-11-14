1b9da9a1752075a5bb6fd947458c6347ae596c7e
NGENG CHHENGKIM <NGENG CHHENGKIM@192.168.6.147>
2014-11-14 15:28:44 +0700

    Thêm NhanVienDal

--- src/controller/Sieuthi.java
+++ src/controller/Sieuthi.java
@@ -19,7 +19,7 @@ public class Sieuthi extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/gui/TimNcc.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/TimNc.fxml"));
         
         Scene scene = new Scene(root);
         
